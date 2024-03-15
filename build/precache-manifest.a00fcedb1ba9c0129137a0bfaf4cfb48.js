2d78894aeb00c7813de561f7068480aef7eb4ce7
krupesh3009 <69378129+krupesh3009@users.noreply.github.com>
2024-03-15 20:12:02 +0530

    Add files via upload

---
+++ build/precache-manifest.a00fcedb1ba9c0129137a0bfaf4cfb48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccb517695ee1a6b3829c3bd933b971d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aa9166257149926e771",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5b8b7829277ab46e2156",
+    "url": "/static/js/2.b09bda3f.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.b09bda3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa9166257149926e771",
+    "url": "/static/js/main.6d29e2d3.chunk.js"
+  },
+  {
+    "revision": "3afce78c2a82586439fa",
+    "url": "/static/js/runtime-main.c7513b00.js"
+  }
+]);
